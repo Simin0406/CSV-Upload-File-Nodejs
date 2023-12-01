ee0fda9a18f751044ec9da38eb7ed7df366c4407
Simin0406 <75030551+Simin0406@users.noreply.github.com>
2023-12-01 21:05:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
-require('dotenv').config()
+/** ------------------ IMPORTING PACKAGE ------------------ **/
 const express = require('express');
+const port = 8000;
 const app = express();
-// const db = require('./config/mongoose');
-const expressLayouts = require('express-ejs-layouts');
-const session = require('express-session');
-const flash = require('connect-flash');
-const customeMW = require('.//config/middleware');
+const path = require('path');
 
-//middleware to use assets
-app.use(express.static('./assets'));
-app.use(express.urlencoded());
+const expressLayouts = require('express-ejs-layouts');
+const csv = require('csv-parser');
+const db = require("./config/mongoose");
+const bodyParser = require('body-parser');
+
+// setting layouts
 app.use(expressLayouts);
 
-//extract styles and scripts from layouts
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+// middleware for body-parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
 
+//accesing static files from assets folder
+app.use(express.static('./assets'));    
 
-//setting view engine as ejs
-app.set('view engine', 'ejs');
-app.set('views', './views');
+//setting up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-// ************************  Database Connection  **********************************//
-const {connectMonggose} = require('./config/mongoose')
-connectMonggose();
-
-
-//to create an duse sessions
-app.use(session({
-  secret: process.env.SECRET,
-  saveUninitialized: true,
-  resave: true
-}));
-
-//using connect-flash to display flash notification in FE
-app.use(flash());
-app.use(customeMW.setFlash);
-
-//router
+// setting up routes
 app.use('/', require('./routes'));
 
-// ************************   Port Start   ********************************//
-const PORT = process.env.PORT || 8500;
-app.listen(PORT,()=>{
-    console.log(`My server start on this port ${PORT}`)
-})
+// directing the app in the given port 
+app.listen(port, function(err) {
+    if(err) {
+        console.log('Error', err);
+        return;
+    }
+    console.log('Server is up and running on port: ', port);
 
+});
